e54ba22a5a5caea10339b6cd3f2d53169bbf99da
Serhii Soloviov <115696868+Serhii-Soloviov@users.noreply.github.com>
2023-03-29 22:43:06 +0300

    edit index

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const contactOperation = require("./contacts");
+//--------------------Command----------------------------//
 const { Command } = require("commander");
 const program = new Command();
+
+//---------------------Концпект/HW--------------------------//
+// const argv = require("yargs").argv;
+
+//-------------------Приклад з відео--------------------//
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
@@ -31,6 +39,8 @@ async function invokeAction({ action, id, name, email, phone }) {
       console.warn("\x1B[31m Unknown action type!");
   }
 }
+//--------------------Command----------------------------//
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -41,3 +51,13 @@ program
 program.parse(process.argv);
 const argv = program.opts();
 invokeAction(argv);
+
+//-------------------Приклад з відео--------------------//
+
+// const arr = hideBin(process.argv);
+// const { argv } = yargs(arr);
+// // const {argv} = yargs(process.argv.slice(2));
+// invokeAction(argv);
+//---------------------Концпект/HW--------------------------//
+
+// invokeAction(argv);
